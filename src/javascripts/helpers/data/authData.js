5fdfc2e822725a9684752535d9cbd13e59d9f79b
Dev Davis <sean.dev.davis@gmail.com>
2019-05-28 19:40:11 -0500

    auth data working

--- src/javascripts/helpers/data/authData.js
+++ src/javascripts/helpers/data/authData.js
@@ -6,6 +6,8 @@ const authLogin = document.getElementById('navbar-button-auth');
 const logoutNavbar = document.getElementById('navbar-button-logout');
 const myMovies = document.getElementById('navbar-button-history');
 
+/* adds and takes away elements from the page and navbar for when the user is logged in or not
+and add takes it away from the page and remove adds it to the page */
 const checkLoginStatus = () => {
   firebase.auth().onAuthStateChanged((user) => {
     if (user) {
