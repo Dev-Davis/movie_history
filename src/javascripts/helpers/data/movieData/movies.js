dde7156c3aead826704a97ed17dde6afc36beaef
Dev Davis <sean.dev.davis@gmail.com>
2019-06-05 13:43:05 -0500

    got the watchList page switching

--- src/javascripts/helpers/data/movieData/movies.js
+++ src/javascripts/helpers/data/movieData/movies.js
@@ -23,18 +23,39 @@ const createMovie = (e) => {
       document.getElementById('rating').value = '';
       document.getElementById('movies').classList.remove('hide');
       document.getElementById('new-movie').classList.add('hide');
+      document.getElementById('createMovieButton').classList.remove('hide');
+      document.getElementById('backButton').classList.add('hide');
+      document.getElementById('backButton').classList.add('hide');
+      // the movieStringBuilder adds the actual movie to the site
       movieStringBuilder(); // eslint-disable-line no-use-before-define
     })
     .catch(err => console.error('no movies added', err));
 };
 
+/* This function is for  when you click the movie button the form to add a new movie appears.
+Once you've entered the information, the form will disappear and you will return to the movies home page */
+
 const addMovieButton = () => {
   document.getElementById('movies').classList.add('hide');
   document.getElementById('new-movie').classList.remove('hide');
+  document.getElementById('toWatch').classList.add('hide');
+  document.getElementById('createMovieButton').classList.add('hide');
+  document.getElementById('backButton').classList.remove('hide');
   document.getElementById('saveNewMovie').addEventListener('click', createMovie);
 };
 
-// How to add to the movieUser in firebase database
+/* This function is for if you go to the add movie page and want to return to the main movie page without
+entering any information to prevent you from adding blank information to the database. */
+
+const backToMovie = () => {
+  document.getElementById('movies').classList.remove('hide');
+  document.getElementById('new-movie').classList.add('hide');
+  document.getElementById('createMovieButton').classList.remove('hide');
+  document.getElementById('backButton').classList.add('hide');
+};
+
+/* How to add to the movieUser in firebase database. The newUserMovie object is the information that will be
+entered into the firebase database. */
 
 const addMovieToWatchList = (e) => {
   e.preventDefault();
@@ -47,16 +68,33 @@ const addMovieToWatchList = (e) => {
   watch.addNewUserMovie(newUserMovie);
 };
 
-const addWatchList = () => {
+/* The watchListButton function attaches to each button on each card to add the click event to them to perform
+a certain function. */
+
+
+// The watchListView function simply hides the main movies page and shows your watch list. **WIP**
+
+const watchListView = () => {
+  document.getElementById('movies').classList.add('hide');
+  document.getElementById('toWatch').classList.remove('hide');
+  document.getElementById('createMovieButton').classList.add('hide');
+  document.getElementById('backButton').classList.remove('hide');
+};
+
+const watchListButton = () => {
   const buttons = document.querySelectorAll('.watchBtn');
 
   buttons.forEach((button) => {
     button.addEventListener('click', addMovieToWatchList);
+    button.addEventListener('click', watchListView);
   });
 };
+
+// The addEvents function adds event listeners to the buttons and the functionality comes from the functions.
+
 const addEvents = () => {
   document.getElementById('createMovieButton').addEventListener('click', addMovieButton);
-  addWatchList();
+  document.getElementById('backButton').addEventListener('click', backToMovie);
 };
 
 /* Creates the domString to display the movies on the webpage. The movies.getMovies()
@@ -69,12 +107,12 @@ const movieStringBuilder = () => {
     domString += '<div class="row">';
     movies.forEach((movie) => {
       domString += '<div class="col-lg-3 col-md-4 col-sm-2">';
-      domString += '<div class="card text-center border" style="width: 15rem;">';
+      domString += '<div class="card text-center border" style="width: 22rem;">';
+      domString += `<h4 class="movieTitle">${movie.title}</h4>`;
       domString += `<img src="${movie.imgUrl}" id="poster" class="card-img-top" alt="Movie Image">`;
       domString += '<div class="card-body">';
-      domString += `<h4 class="movieTitle">${movie.title}</h4>`;
-      domString += `<p class="card-text rating">Rated ${movie.movieRating}</p>`;
-      domString += `<div id="${movie.id}" class="btn btn-primary watchBtn">Watch Later</div>`;
+      domString += `<h4 class="card-text rating">Rated ${movie.movieRating}</h4>`;
+      domString += `<div id="${movie.id}" class="btn btn-warning watchBtn">Watch Later</div>`;
       domString += '</div>';
       domString += '</div>';
       domString += '</div>';
@@ -82,8 +120,32 @@ const movieStringBuilder = () => {
     domString += '</div>';
     util.printToDom('moviesDisplay', domString);
     addEvents();
+    watchListButton();
   })
     .catch(err => console.error('Could not get movies', err));
 };
 
-export default { movieStringBuilder };
+const watchStringBuilder = () => {
+  watch.getUserMovie().then((watchList) => {
+    let domString = '';
+    domString += '<div class="row">';
+    watchList.forEach((movie) => {
+      domString += '<div class="col-lg-3 col-md-4 col-sm-2">';
+      domString += '<div class="card text-center border" style="width: 15rem;">';
+      domString += `<h4 class="movieTitle">${movie.title}</h4>`;
+      domString += `<img src="${movie.imgUrl}" id="poster" class="card-img-top" alt="Movie Image">`;
+      domString += '<div class="card-body">';
+      domString += `<h4 class="card-text rating">Rated ${movie.movieRating}</h4>`;
+      domString += `<div id="${movie.id}" class="btn btn-warning watchBtn">Watch Later</div>`;
+      domString += '</div>';
+      domString += '</div>';
+      domString += '</div>';
+      addEvents();
+    });
+    domString += '</div>';
+    util.printToDom('watchList', domString);
+  })
+    .catch(err => console.error('no watch list returned', err));
+};
+
+export default { movieStringBuilder, watchStringBuilder };
